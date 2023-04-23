d4da58958d9916fb5f7cfa9b16f597ddaaff7648
vnsoft <vnsoft.he@gmail.com>
2023-04-23 08:46:23 +0800

    update

--- og_database_comment/__manifest__.py
+++ og_database_comment/__manifest__.py
@@ -16,6 +16,7 @@
     'demo': [],
     'application': True,
     'installable': True,
+    'auto_install': False,
     'pre_init_hook': 'pre_init_hook',
     'license': 'LGPL-3',
     'price': '0.0',
